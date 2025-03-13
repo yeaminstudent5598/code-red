438a1678c5e3d216189369874561eddfbd11a7cd
nmmaharaz <nmmaharaz@gmail.com>
2025-03-13 23:43:35 +0600

    complete footer section of the code red project

--- src/app/(component)/shered/Footer.tsx
+++ src/app/(component)/shered/Footer.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 function Footer() {
   return (
     <div className="sticky bottom-0 z-50 bg-white shadow-2xl shadow-gray-900">
-      <div className="flex w-11/12 mx-auto justify-between flex-row min-lg:hidden ">
+      <div className="flex w-11/12 mx-auto justify-between flex-row min-lg:hidden">
         {sidebarLinks?.map((link) => {
           return (
             <Link
