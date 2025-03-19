98e80f0cde00c1594a6e11fc4265593b5b0f8b2d
AyshaUrmi0 <ayshaismail021@gmail.com>
2025-03-19 22:57:46 +0600

    login button changed

--- src/components/auth/LoginButton.tsx
+++ src/components/auth/LoginButton.tsx
@@ -4,7 +4,7 @@ import { signIn } from "next-auth/react";
 export default function LoginButton() {
   return (
     <div>
-      <button onClick={() => signIn()} className="bg-green-500 py-1 px-4 rounded-2xl text-lg font-semibold cursor-pointer hover:ring-2 ring-blue-500 duration-200">Sign in</button>
+      <button onClick={() => signIn()} className="bg-green-600 py-1 px-4 rounded-2xl text-lg font-semibold cursor-pointer hover:ring-2 ring-blue-500 duration-200">Sign in</button>
     </div>
   );
 }
