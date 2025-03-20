dd4a8f576f7b128cb8cc51bf71f4b07f5ef0db01
yeaminstudent5598 <yeaminstudent5598@gmail.com>
2025-03-20 11:44:58 +0600

    profile section file structure done

---
+++ src/app/(component)/Profile/layout.tsx
@@ -0,0 +1,17 @@
+import "./globals.css";
+
+export default function DashboardLayout({
+    children,
+  }: {
+    children: React.ReactNode
+  }) {
+    return (
+      <html lang="en">
+        <body>
+          {/* Layout UI */}
+          {/* Place children where you want to render a page or nested layout */}
+          <main>{children}</main>
+        </body>
+      </html>
+    )
+  }
