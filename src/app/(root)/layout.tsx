1778c83902e04caa8b86ec5ae5814e646420bf2a
Sheikh Muhammad Antor <sheikhmuhammadantor@gmail.com>
2025-03-20 23:08:12 +0600

    [Update] : Refactor layout structure to include LayoutWrapper; update basic registration form.

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -2,10 +2,8 @@ import type { Metadata } from "next";
 import NextAuthSessionProvider from '@/providers/NextAuthSessionProvider';
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import LeftSideBar from "../(component)/shered/LeftSideBar";
-import RightSideBar from "../(component)/shered/RightSideBar";
 import Navbar from "../(component)/shered/Navbar";
-import Footer from "../(component)/shered/Footer";
+import LayoutWrapper from '@/components/auth/LayoutWrapper';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,23 +25,15 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
   return (
     <html lang="en">
       <NextAuthSessionProvider>
         <body className={`${geistSans.variable} ${geistMono.variable} antialiased text-black`}>
-          <Navbar></Navbar>
-          <div className=" bg-gray-100 pt-6 ">
-            <div className="flex justify-between w-11/12 mx-auto">
-              <LeftSideBar></LeftSideBar>
-              <main className="w-full mx-auto lg:mx-6 lg:w-6/12">
-                <div className="min-h-screen">
-                  {children}
-                </div>
-              </main>
-              <RightSideBar></RightSideBar>
-            </div>
-            <Footer></Footer>
-          </div>
+          <Navbar />
+          <LayoutWrapper>
+            {children}
+          </LayoutWrapper>
         </body>
       </NextAuthSessionProvider>
     </html>
--- src/components/auth/RegisterFrom.tsx
+++ src/components/auth/RegisterFrom.tsx
@@ -1,33 +1,41 @@
 "use client";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
+import register from '@/image/auth/signup.jpg'
 
 export default function RegisterFrom() {
     const router = useRouter();
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const formData = new FormData(e.currentTarget);
+        const email = formData.get("email") as string;
+        const password = formData.get("password") as string;
+        console.log(email, password);
+        // e.currentTarget.reset();
+    }
 
     return (
         <>
             <div className="flex justify-center mt-16">
                 <div style={{ minWidth: "30%" }}>
-                    <div className="flex min-h-full shadow-lg flex-1 flex-col justify-center px-6 
-py-12 lg:px-8 bg-white">
+                    <div className="flex min-h-full shadow-lg flex-1 flex-col justify-center px-6 py-12 lg:px-8 bg-white">
+
                         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
                             <div className="flex justify-center">
-                                <Image src="/login.gif" height={100} width={100} alt="fig" />
+                                <Image src={register} height={150} width={150} alt="fig" />
                             </div>
-                            <h2 className="mt-1 text-center text-2xl font-bold leading-9 
-tracking-tight text-gray-900">
+                            <h2 className="mt-1 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
                                 Sign in to your account
                             </h2>
                         </div>
 
                         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-                            <form className="space-y-6" action="#" method="POST">
+                            <form onSubmit={handleSubmit} className="space-y-6">
                                 <div>
                                     <label
                                         htmlFor="email"
-                                        className="block text-sm font-medium leading-6 text-gray-900"
-                                    >
+                                        className="block text-sm font-medium leading-6 text-gray-900">
                                         Email address
                                     </label>
                                     <div className="mt-2">
@@ -37,10 +45,7 @@ tracking-tight text-gray-900">
                                             type="email"
                                             autoComplete="email"
                                             required
-                                            className="block w-full rounded-md border-0 py-1.5 text-gray-900 
-shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 
-focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                                        />
+                                            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
                                     </div>
                                 </div>
 
@@ -48,15 +53,11 @@ focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                     <div className="flex items-center justify-between">
                                         <label
                                             htmlFor="password"
-                                            className="block text-sm font-medium leading-6 text-gray-900"
-                                        >
+                                            className="block text-sm font-medium leading-6 text-gray-900">
                                             Password
                                         </label>
                                         <div className="text-sm">
-                                            <a
-                                                href="#"
-                                                className="font-semibold text-indigo-600 hover:text-indigo-500"
-                                            >
+                                            <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
                                                 Forgot password?
                                             </a>
                                         </div>
@@ -68,22 +69,15 @@ focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                             type="password"
                                             autoComplete="current-password"
                                             required
-                                            className="block w-full rounded-md border-0 py-1.5 text-gray-900 
-shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 
-focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                                        />
+                                            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
                                     </div>
                                 </div>
 
                                 <div>
                                     <button
                                         type="submit"
-                                        className="flex w-full justify-center rounded-md bg-indigo-600
- px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 
-focus-visible:outline focus-visible:outline-offset-2 
-focus-visible:outline-indigo-600"
-                                    >
-                                        Sign in
+                                        className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                                        Sign up
                                     </button>
                                 </div>
                             </form>
@@ -91,10 +85,8 @@ focus-visible:outline-indigo-600"
                             <p className="mt-10 text-center text-sm text-gray-500">
                                 Not a member?{" "}
                                 <span
-                                    className="font-semibold leading-6 text-indigo-600 
-hover:text-indigo-500 cursor-pointer"
-                                    onClick={() => { router.push("/signup") }}
-                                >
+                                    className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500 cursor-pointer"
+                                    onClick={() => { router.push("/signup") }}>
                                     Start a 14 day free trial
                                 </span>
                             </p>
