cf70b1fdf11bfb80d47f7b15d8ddfe1cd87108f6
AyshaUrmi0 <ayshaismail021@gmail.com>
2025-04-11 15:12:32 +0600

    Navbar and main layout changed

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -75,10 +75,10 @@ export default function Navbar() {
                                     tabIndex={0}
                                     className="menu menu-sm dropdown-content bg-white text-black rounded-box z-1 mt-3 w-52 p-2 shadow">
                                     <li>
-                                        <a className="justify-between">
+                                    <Link href={"/profile"} className="justify-between">
                                             Profile
                                             <span className="badge">New</span>
-                                        </a>
+                                        </Link>
                                     </li>
                                     <li><a>Settings</a></li>
                                     <li><a>Message</a></li>
